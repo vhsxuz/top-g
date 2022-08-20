f3d5c76e59a9a05d4026eefc4a363253c89a37e9
Andreas Alexander <ndre19223@gmail.com>
2022-08-20 14:11:58 +0700

    create gym info card

--- client/src/routes/Routing.tsx
+++ client/src/routes/Routing.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
 import ErrorPage from '../pages/Error-Page';
+import GymList from '../pages/gym/Gym-List';
 
 function Routing() {
   return (
@@ -11,6 +12,7 @@ function Routing() {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
+      <Route path='/gym-list' element={<GymList />} />
       <Route path='*' element={<ErrorPage />} />
     </Routes>
   )
