8dc2ec26c7f121323d2c80fa221b3ee687b0e278
Andreas Alexander <ndre19223@gmail.com>
2022-08-21 11:05:17 +0700

    create logout feature on navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,24 +3,42 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  let token = document.cookie.slice(6);
+
+  const logout = () => {
+    token = "";
+    document.cookie = "token="
+    console.log(token);
+  }
+
   return (
     <Flex minWidth='max-content' alignItems='center' gap='2' m='4'>
       <Box p='2'>
         <Heading size='md'>Top G</Heading>
       </Box>
       <Spacer />
-      <ButtonGroup gap='2'>
-        <Link to='/register'>
-          <Button colorScheme='teal' size='sm'>
-            Register
-          </Button>
-        </Link>
-        <Link to="/login">
-          <Button colorScheme='teal' size='sm'>
-            Login
-          </Button>
-        </Link>
-      </ButtonGroup>
+      {
+        token ?
+          <Link to='/'>
+            <Button colorScheme='teal' size='sm' onClick={logout}>
+              Logout
+            </Button>
+          </Link>
+          :
+          <ButtonGroup gap='2'>
+            <Link to='/register'>
+              <Button colorScheme='teal' size='sm'>
+                Register
+              </Button>
+            </Link>
+            <Link to="/login">
+              <Button colorScheme='teal' size='sm'>
+                Login
+              </Button>
+            </Link>
+          </ButtonGroup>
+      }
+
     </Flex>
   )
 }
